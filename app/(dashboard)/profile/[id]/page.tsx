f3173d536a1f2322106a21fceb7b1f1a6a1afd26
Brooklyn7x <shubham.jaiswal020@gmail.com>
2024-05-28 19:44:46 +0530

    add lazy loading & performance

--- app/(dashboard)/profile/[id]/page.tsx
+++ app/(dashboard)/profile/[id]/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { ProfileTabs } from "../../../../components/profile/profile-tabs";
 import { useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
-import ProfileHeader from "@/components/profile/profile-header";
-import ProfileFollower from "@/components/profile/profile-follower";
-import ProfileFollowingButton from "@/components/profile/profile-following-button";
+import dynamic from "next/dynamic";
+import { ProfileTabs } from "@/components/profile/profile-tabs";
+
+const ProfileHeader = dynamic(
+  () =>
+    import("@/components/profile/profile-header").then((mod) => mod.default),
+  { ssr: false }
+);
+const ProfileFollower = dynamic(
+  () =>
+    import("@/components/profile/profile-follower").then((mod) => mod.default),
+  { ssr: false }
+);
+const ProfileFollowingButton = dynamic(
+  () =>
+    import("@/components/profile/profile-following-button").then(
+      (mod) => mod.default
+    ),
+  { ssr: false }
+);
 
 type Props = {
   params: {
@@ -16,6 +32,8 @@ type Props = {
 const ProfileIdPage = ({ params }: Props) => {
   const userId = params.id;
   const user = useQuery(api.threadUser.getByuser, { userId });
+
+  if (!user) return <div>Loading...</div>;
 
   return (
     <div className="p-2 mx-2">
